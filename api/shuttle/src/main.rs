c628c42b7f2da3b0272e54e2766c0b1256cbd80b
codinlog <codinlog@foxmail.com>
2023-07-20 22:50:10 +0800

    add database connect

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -5,7 +5,12 @@ async fn hello_world() -> &'static str {
 }
 
 #[shuttle_runtime::main]
-async fn axum() -> shuttle_axum::ShuttleAxum {
+async fn axum(
+    #[shuttle_shared_db::Postgres(
+        local_uri = "postgres://postgres:{secrets.PASSWORD}@127.0.0.1:5432/postgres"
+    )]
+    _pool: sqlx::PgPool,
+) -> shuttle_axum::ShuttleAxum {
     let router = Router::new().route("/", get(hello_world));
 
     Ok(router.into())
