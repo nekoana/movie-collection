b798ec9b38374730599d0c8bf1fa410af287dc83
codinlog <codinlog@foxmail.com>
2023-07-22 11:51:21 +0800

    add serde dependency and derive traits

--- shared/src/film_model.rs
+++ shared/src/film_model.rs
@@ -1,5 +1,4 @@
-use chrono::TimeZone;
-
+#[derive(serde::Serialize, serde::Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Film {
     pub id: uuid::Uuid,
     pub title: String,
@@ -8,4 +7,12 @@ pub struct Film {
     pub poster: String,
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
     pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct CreateFilm {
+    pub title: String,
+    pub director: String,
+    pub year: u16,
+    pub poster: String,
 }
