3c3e1fca8297afae2be7087e602b501841e70a2c
codinlog <codinlog@foxmail.com>
2023-07-22 13:43:27 +0800

    add film repository trait

---
+++ api/lib/src/film_repository/mod.rs
@@ -0,0 +1,16 @@
+pub type FilmErr = String;
+pub type FilmResult<T> = Result<T, FilmErr>;
+
+pub trait FilmRepository {
+    async fn list_films(&self) -> FilmResult<Vec<shared::film_model::Film>>;
+    async fn query_film_by_id(&self, id: &uuid::Uuid) -> FilmResult<shared::film_model::Film>;
+    async fn create_film(
+        &self,
+        film: &shared::film_model::CreateFilm,
+    ) -> FilmResult<shared::film_model::Film>;
+    async fn update_film(
+        &self,
+        film: &shared::film_model::Film,
+    ) -> FilmResult<shared::film_model::Film>;
+    async fn delete_film_by_id(&self, id: &uuid::Uuid) -> FilmResult<uuid::Uuid>;
+}
--- api/lib/src/lib.rs
+++ api/lib/src/lib.rs
@@ -1,2 +1,6 @@
+#![feature(async_fn_in_trait)]
+
+pub mod film_repository;
 pub mod films;
+
 pub mod health;
